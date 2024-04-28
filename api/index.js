eebb4aa9118c399abd2d3a4a8eca78ee2455a32e
DIWAKAR <didk9675@gmail.com>
2024-04-29 00:25:57 +0530

    a

--- api/index.js
+++ api/index.js
@@ -75,8 +75,8 @@ app.get("/", (req, res) => {
 app.use(ErrorHandler);
 
 // create server
-const server = app.listen(process.env.PORT, () => {
-    console.log(`Server is running on http://localhost:${process.env.PORT}`);
+const server = app.listen(8000, () => {
+    console.log(`Server is running on http://localhost:8000`);
 });
 
 // unhandled promise rejection
